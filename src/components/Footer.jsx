4895da0483ffbc89deef39d72e2d3368ee37cd3d
Carlo Costa <carlo.costa92@gmail.com>
2025-04-03 16:58:23 +0200

    feat: second section footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,90 @@
+import digitalComics from '../assets/img/buy-comics-digital-comics.png';
+import merchandise from '../assets/img/buy-comics-merchandise.png';
+import subscriptions from '../assets/img/buy-comics-subscriptions.png';
+import shopLocator from '../assets/img/buy-comics-shop-locator.png';
+import powerVisa from '../assets/img/buy-dc-power-visa.svg';
+import bigLogo from '../assets/img/dc-logo-bg.png';
+
 export default function Footer() {
-    return (<footer>sono il footer della pagina</footer>)
+    return (<footer>
+        <div className="blue-banner">
+            <div className="card">
+                <img src={digitalComics} alt="Fumetti Digitali" />
+                <span>FUMETTI DIGITALI</span>
+            </div>
+            <div className="card">
+                <img src={merchandise} alt="Merchandising DC" />
+                <span>MERCHANDISING DC</span>
+            </div>
+            <div className="card">
+                <img src={subscriptions} alt="Abbonamenti" />
+                <span>ABBONAMENTI</span>
+            </div>
+            <div className="card">
+                <img src={shopLocator} alt="Trova Fumetteria" />
+                <span>TROVA FUMETTERIA</span>
+            </div>
+            <div className="card">
+                <img src={powerVisa} alt="DC Power Visa" />
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+        <div className="main-footer-container"></div>
+        <section className='mainFooter'>
+            <div className="ft-left">
+                <div className="footer-links-column">
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                    </ul>
+                </div>
+                <div className="footer-links-column">
+                    <h3>DC</h3>
+                    <ul>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy Policy (New)</a></li>
+                        <li><a href="#">Advertising</a></li>
+                    </ul>
+                </div>
+                <div className="footer-links-column">
+                    <h3>SITES</h3>
+                    <ul>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+
+                    </ul>
+                </div>
+                <div className="footer-links-column">
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Collectibles</a></li>
+                    </ul>
+
+                </div>
+            </div>
+            <div className="ft-right">
+                <img src={bigLogo} alt="biglogo" />
+            </div>
+        </section>
+
+        <section></section>
+    </footer>)
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,20 @@ import Logo from "./logo";
 
 export default function Header() {
     return (<header>
-        <div>
-            <Logo />
-            <ul>
-                <li>CHARACTERS</li>
-                <li>COMICS</li>
-                <li>MOVIES</li>
-                <li>TV</li>
-                <li>GAMES</li>
-                <li>COLLECTIBLES</li>
-                <li>VIDEOS</li>
-                <li>FANS</li>
-                <li>NEWS</li>
-                <li>SHOPS</li>
-            </ul>
-        </div>
+
+        <Logo />
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOPS</li>
+        </ul>
+
     </header>)
 };
