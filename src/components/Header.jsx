8d1f4968c79baae7b24278c3a45aa80de4107d4d
Carlo Costa <carlo.costa92@gmail.com>
2025-04-04 14:49:51 +0200

    feat: create link array

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,54 @@
 import Logo from "./logo";
 import Menu from "./Menu.jsx";
+const link = [
+    {
+        text: "CHARACTERS",
+        href: "#"
+    },
+    {
+        text: "COMICS",
+        href: "#"
+    },
+    {
+        text: "MOVIES",
+        href: "#"
+    },
+    {
+        text: "TV",
+        href: "#"
+    },
+    {
+        text: "GAMES",
+        href: "#"
+    },
+    {
+        text: "COLLECTIBLES",
+        href: "#"
+    },
+    {
+        text: "VIDEOS",
+        href: "#"
+    },
+    {
+        text: "FANS",
+        href: "#"
+    },
+    {
+        text: "NEWS",
+        href: "#"
+    },
+    {
+        text: "SHOPS",
+        href: "#"
+    },
+];
 
 export default function Header() {
+
     return (<header>
 
         <Logo />
-        <Menu />
+        <Menu link={link} />
 
     </header>)
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,12 @@
-export default function Menu() {
-    <ul>
-        <li><a href="#">CHARACTERS</a></li>
-        <li><a href="#">COMICS</a></li>
-        <li><a href="#">MOVIES</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">GAMES</a></li>
-        <li><a href="#">COLLECTIBLES</a></li>
-        <li><a href="#">VIDEOS</a></li>
-        <li><a href="#">FANS</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">SHOPS</a></li>
-    </ul>
+export default function Menu(props) {
+
+    return (
+        <ul>
+            {props.link.map((link) => (
+                <li >
+                    <a href={link.href}>{link.text}</a>
+                </li>
+            ))}
+        </ul>
+    );
 };
