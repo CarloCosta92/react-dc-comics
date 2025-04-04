cd9f65daf2eb03c8f781277cbad39d13c9bb2a81
Carlo Costa <carlo.costa92@gmail.com>
2025-04-04 09:27:04 +0200

    feat: set hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,36 +40,36 @@ export default function Footer() {
                 <div className="footer-links-column">
                     <h3>DC COMICS</h3>
                     <ul>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
                         <li><a href="#">Movies</a></li>
                         <li><a href="#">TV</a></li>
                         <li><a href="#">Games</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
                     </ul>
                 </div>
                 <div className="footer-links-column">
                     <h3>DC</h3>
                     <ul>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy Policy (New)</a></li>
+                        <li><a href="#">Termo Of Use</a></li>
+                        <li><a href="#">Privacy Policy(New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
                         <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscription</a></li>
+                        <li><a href="#">Talent WOrkshop</a></li>
+                        <li><a href="#">Cpsc Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Hop Helps</a></li>
+                        <li><a href="#">Contact Us</a></li>
                     </ul>
                 </div>
                 <div className="footer-links-column">
                     <h3>SITES</h3>
                     <ul>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
                         <li><a href="#">DC Kids</a></li>
                         <li><a href="#">DC Universe</a></li>
                         <li><a href="#">DC Power Visa</a></li>
@@ -80,7 +80,7 @@ export default function Footer() {
                     <h3>SHOP</h3>
                     <ul>
                         <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Collectibles</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
                     </ul>
 
                 </div>
