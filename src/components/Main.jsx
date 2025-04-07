2d7d4a54e960779637962c09e1528814760c6a88
Carlo Costa <carlo.costa92@gmail.com>
2025-04-07 16:52:51 +0200

    feat: set jumbotron image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,10 @@ import Card from "./card"
 
 export default function Main() {
     return (<main>
+        <div className="jumbo">
+        </div>
         <div className="jumbotron">
+
             <Card />
         </div>
     </main>)
