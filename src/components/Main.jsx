8ed2add5cb147e460a11ed62bf79d9be1ee628bd
Carlo Costa <carlo.costa92@gmail.com>
2025-04-07 14:58:54 +0200

    feat: create cards in page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import Card from "./card"
 export default function Main() {
     return (<main>
         <div className="jumbotron">
-            <h2>Content goes here</h2>
+
             <Card />
         </div>
     </main>)
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,11 +2,11 @@ import comics from "./../data/comics.js"
 
 export default function Card() {
     return (
-        <div>
+        <div className="card-container">
             {comics.map((comic) => (
-                <div key={comic.id}>
-                    <img src={comic.thumb} alt={comic.title} />
-                    <span>{comic.series}</span>
+                <div key={comic.id} className="cards" >
+                    <img src={comic.thumb} alt={comic.title} className="img-card" />
+                    <div>{comic.series}</div>
                 </div>
 
             ))}
